313979347453129a2a77897302ce65916bf6c1c7
Christian Metzlaff <christian.metzlaff@student.kit.edu>
2015-07-14 14:30:52 +0200

    Refactored namings

--- flavourresponse/matrixinversion.py
+++ flavourresponse/matrixinversion.py
@@ -1,49 +1,46 @@
 import ROOT
 import numpy as np
-from pprint import pprint
 
 
-
-class jet2DTagging:
+class MatrixInversion:
 	zones = ['1Q', '3C', '4B', '2G']
 	flavours = ['uds', 'c', 'b', 'g']
 
-	def __init__(self, mc_plots, data_plots = {}, debug = False):
-		self._plots = mc_plots
-		self._plots.update(data_plots)
+	def __init__(self, plots, debug=False):
+		self._plots = plots
 		self._debug = debug
 
-	def getMeanMpfValues(self, prefix):
-		if not prefix in ['MC', 'Data']:
+	def get_mean_mpf_values(self, prefix):
+		if prefix not in ['MC', 'Data']:
 			raise ValueError
 
 		mean_mpf_values = []
 		mean_mpf_values_up = []
 		mean_mpf_values_down = []
 		for zone in self.zones:
-			plot = self._plots[prefix+"_MPF_Zone"+zone]
+			plot = self._plots[prefix + "_MPF_Zone" + zone]
 			mean_mpf_values.append(plot.GetMean())
 			mean_mpf_values_up.append(mean_mpf_values[-1] + plot.GetMeanError())
 			mean_mpf_values_down.append(mean_mpf_values[-1] - plot.GetMeanError())
 
 		return np.array(mean_mpf_values), np.array(mean_mpf_values_up), np.array(mean_mpf_values_down)
 
-	def getFlavourFractions(self):
+	def get_flavour_fractions(self):
 		flavour_fractions = []
 		for zone in self.zones:
 			zone_fractions = []
-			all_plot = self._plots["MC_MPF_Zone"+zone]
+			all_plot = self._plots["MC_MPF_Zone" + zone]
 			sum_all = all_plot.Integral()
 			for flavour in self.flavours:
-				plot = self._plots["MC_MPF_Zone"+zone+"_"+flavour]
+				plot = self._plots["MC_MPF_Zone" + zone + "_" + flavour]
 				flavour_sum = plot.Integral()
 				zone_fractions.append(flavour_sum / sum_all)
 			flavour_fractions.append(zone_fractions)
 		return np.array(flavour_fractions)
 
-	def getFlavourResponses(self, prefix):
-		mean_mpf_values, mean_mpf_values_up, mean_mpf_values_down = self.getMeanMpfValues(prefix)
-		flavour_fractions = self.getFlavourFractions()
+	def get_flavour_responses(self, prefix):
+		mean_mpf_values, mean_mpf_values_up, mean_mpf_values_down = self.get_mean_mpf_values(prefix)
+		flavour_fractions = self.get_flavour_fractions()
 
 		response_for_flavour = np.linalg.solve(flavour_fractions, mean_mpf_values)
 		response_for_flavour_up = np.linalg.solve(flavour_fractions, mean_mpf_values_up)
@@ -51,13 +48,13 @@ class jet2DTagging:
 
 		return response_for_flavour, response_for_flavour_up, response_for_flavour_down
 
-	def getFlavourResponsePlot(self, prefix):
+	def get_flavour_response_plot(self, prefix):
 		result_plot = ROOT.TGraphAsymmErrors()
-		result_plot.SetName(prefix+"_MPF_Zones_Flavour_Response")
+		result_plot.SetName(prefix + "_MPF_Zones_Flavour_Response")
 
-		response_for_flavour, response_for_flavour_up, response_for_flavour_down = self.getFlavourResponses(prefix)
+		response_for_flavour, response_for_flavour_up, response_for_flavour_down = self.get_flavour_responses(prefix)
 		for i in xrange(len(self.flavours)):
-			result_plot.SetPoint(i, i+1, response_for_flavour[i])
+			result_plot.SetPoint(i, i + 1, response_for_flavour[i])
 			result_plot.SetPointEYhigh(i, response_for_flavour_up[i] - response_for_flavour[i])
 			result_plot.SetPointEYlow(i, response_for_flavour[i] - response_for_flavour_down[i])
 
--- tagging.py
+++ tagging.py
@@ -2,7 +2,8 @@
 
 import ROOT
 import argparse
-from jec.tagging import jet2DTagging
+
+from flavourresponse.matrixinversion import MatrixInversion
 
 parser = argparse.ArgumentParser(description='2DTagging Tool')
 
@@ -17,31 +18,30 @@ data_file = None
 if args.data is not None:
 	data_file = ROOT.TFile(args.data)
 
-mc_plots = {}
-data_plots = {}
-for zone in ['1Q', '3C', '4B', '2G']:
-	nick = "MC_MPF_Zone"+zone
-	mc_plots[nick] = mc_file.Get(nick)
-	for flavour in ['uds', 'c', 'b', 'g']:
-		nick = "MC_MPF_Zone"+zone+"_"+flavour
-		mc_plots[nick] = mc_file.Get(nick)
+plots = {}
+for zone in MatrixInversion.zones:
+	nick = "MC_MPF_Zone" + zone
+	plots[nick] = mc_file.Get(nick)
+	for flavour in MatrixInversion.flavours:
+		nick = "MC_MPF_Zone" + zone + "_" + flavour
+		plots[nick] = mc_file.Get(nick)
 
 	if data_file is not None:
-		nick = "Data_MPF_Zone"+zone
-		data_plots[nick] = data_file.Get(nick)
+		nick = "Data_MPF_Zone" + zone
+		plots[nick] = data_file.Get(nick)
 
-tagging = jet2DTagging(mc_plots, data_plots)
+tagging = MatrixInversion(plots)
 output_file = ROOT.TFile(args.filename, "recreate")
 
-result_mc = tagging.getFlavourResponsePlot('MC')
+result_mc = tagging.get_flavour_response_plot('MC')
 print "Results for MC:"
+
 result_mc.Print()
 result_mc.Write()
 if data_file is not None:
-	result_data = tagging.getFlavourResponsePlot('Data')
+	result_data = tagging.get_flavour_response_plot('Data')
 	print "Results for Data:"
 	result_data.Print()
 	result_data.Write()
 output_file.Close()
 print "Write output to: %s" % (output_file.GetPath())
-
