1e4ce1dbf14b1f6f1c20742a52754a36aab65a47
Christian Metzlaff <christian.metzlaff@student.kit.edu>
2015-07-15 11:01:15 +0200

    Added reStructuredText Doc-Strings to the MatrixInvasion class

--- flavourresponse/matrixinversion.py
+++ flavourresponse/matrixinversion.py
@@ -3,14 +3,37 @@ import numpy as np
 
 
 class MatrixInversion:
+	"""
+
+	:type zones: list[str]
+	:type flavours: list[str]
+	:type _plots: dict[ROOT.TH1]
+	:type _print_results: bool
+	"""
+
 	zones = ['1Q', '3C', '4B', '2G']
 	flavours = ['uds', 'c', 'b', 'g']
 
-	def __init__(self, plots, debug=False):
+	def __init__(self, plots, print_results=False):
+		"""Initialization
+
+		:param plots: Histogramms required for the matrix inversion
+		:type plots: ROOT.TH1
+		:param print_results: Print the results of the matrix inversion
+		:type print_results: bool
+		:rtype: void
+		"""
 		self._plots = plots
-		self._debug = debug
+		self._print_results = print_results
 
 	def get_mean_mpf_values(self, prefix):
+		"""Returns the mean mpf and mpf up/down variation values of each zone
+
+		:param prefix: Prefix of the input plot nicknames (e.g. MC, Data)
+		:type prefix: str
+		:return: Numpy arrays with mpf and mpf up/down variation values of each zone
+		:rtype: (np.array, np.array, np.array)
+		"""
 		if prefix not in ['MC', 'Data']:
 			raise ValueError
 
@@ -26,6 +49,11 @@ class MatrixInversion:
 		return np.array(mean_mpf_values), np.array(mean_mpf_values_up), np.array(mean_mpf_values_down)
 
 	def get_flavour_fractions(self):
+		"""Returns the flavour composition of each zone in monte carlo
+
+		:return: Flavour composition as numpy array
+		:rtype: np.array
+		"""
 		flavour_fractions = []
 		for zone in self.zones:
 			zone_fractions = []
@@ -39,6 +67,13 @@ class MatrixInversion:
 		return np.array(flavour_fractions)
 
 	def get_flavour_responses(self, prefix):
+		"""Calculates the jet response for each flavour
+
+		:param prefix: Prefix of the input plot nicknames (e.g. MC, Data)
+		:type prefix: str
+		:return: Jet response for each flavour and up/down variations
+		:rtype: (np.array, np.array, np.array)
+		"""
 		mean_mpf_values, mean_mpf_values_up, mean_mpf_values_down = self.get_mean_mpf_values(prefix)
 		flavour_fractions = self.get_flavour_fractions()
 
@@ -49,6 +84,13 @@ class MatrixInversion:
 		return response_for_flavour, response_for_flavour_up, response_for_flavour_down
 
 	def get_flavour_response_plot(self, prefix):
+		"""Returns a plot with jet response for each flavour.
+
+		:param prefix: Prefix of the input plot nicknames (e.g. MC, Data)
+		:type prefix: str
+		:return: Plot with jet response for each flavour
+		:rtype: ROOT.TGraphAsymmErrors()
+		"""
 		result_plot = ROOT.TGraphAsymmErrors()
 		result_plot.SetName(prefix + "_MPF_Zones_Flavour_Response")
 
